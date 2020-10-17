50f14ca2bf6e1c7a6cee49129720f37f24afbf10
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-10-17 13:02:59 -0400

    hello you

---
+++ hello_you.py
@@ -0,0 +1,23 @@
+# Ask user for name
+
+name = input("What is your name?: ")
+
+# Ask user for age
+
+age = input("How old are you?: ")
+
+# Ask user for city
+
+city = input("Where do you live?: ")
+
+# Ask user what they enjoy
+
+love = input("What do you like to do?: ")
+
+# Create output text
+
+string = "Your name is {} and you are {} years old. You live in {} and you love {}".format(name, age, city, love)
+
+# Print output to screen
+
+print(string)
