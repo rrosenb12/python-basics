91420b86bac1c048963472b2a1ad62bfd36a9abc
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-12-15 17:19:11 -0500

    email slicer

---
+++ email-slicer.py
@@ -0,0 +1,20 @@
+# get user email address
+
+email = input("What is your email address?: ").strip()
+
+# slice out user name
+
+user = email[:email.index("@")]
+
+# slice out domain name
+
+domain = email[email.index("@") + 1 :]
+
+# format output message
+
+output = "Your username is {} and your domain name is {}".format(user, domain)
+
+# display output message
+
+print(output)
+
