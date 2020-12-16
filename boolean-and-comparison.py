a1dab513546b3d80285716885a8ef1efdf467cb7
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-12-16 09:30:13 -0500

    booleans and comparisons

---
+++ boolean-and-comparison.py
@@ -0,0 +1,19 @@
+B = True
+C = False
+# do not spell booleans lowercase... lowercase boolean is really just a variable
+2 > 3
+# comparison operator ... returns False
+type(2 < 3)
+# returns 'bool'
+2 == 3
+# returns False
+# equality operator: ==
+2 != 3
+# returns True
+# inequality operator: !=
+4 >= 3
+# returns True
+# greater than or equal to operator: >=
+3 <= 4
+# returns True
+# less than or equal to operator: <=
