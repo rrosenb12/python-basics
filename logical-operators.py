972102d7ee18378a56d7c9ec2cc4da1e030e8c25
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-12-30 11:50:45 -0500

    logical operators

---
+++ logical-operators.py
@@ -0,0 +1,44 @@
+not True
+# False
+not False
+# True
+not 2 < 3
+# False
+not 3 > 1
+# False
+not 4 == 3
+# True
+x = 10
+y = 10
+if not y < x:
+    print("it worked")
+C = 10
+D = 5
+if C > 10 and D > 1:
+    print("it worked")
+False and True
+# False
+if not (C > 10 and D > 1):
+    print("it worked")
+not False and True
+# True
+C = 5
+D = -1
+if C > 1 or D > 1:
+    print("it worked")
+True or False
+# True
+if C > 100 or D > 100:
+    print("it worked")
+False or False
+# False
+C = 6
+D = 2
+if (C > 5 and D > 5) or (C > 1 and D > 1):
+    print("it worked")
+False or True
+# True
+if not((C > 5 and D > 5) or (C > 1 and D > 1)):
+    print ("it worked")
+not True
+# False
